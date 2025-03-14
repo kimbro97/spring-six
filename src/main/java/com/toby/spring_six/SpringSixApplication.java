9b48c394bdab35aa2138ddf05c4a533bc33b9d07
kimbro97 <rlagudwog@gmail.com>
2025-03-14 14:20:13 +0900

    project init

---
+++ src/main/java/com/toby/spring_six/SpringSixApplication.java
@@ -0,0 +1,13 @@
+package com.toby.spring_six;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toby/spring_six/SpringSixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.spring_six;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
