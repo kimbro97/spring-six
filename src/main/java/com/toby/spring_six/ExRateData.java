34edbecfb0dcb1a0aee6a1b32a4a739b794b9fa8
kimbro97 <rlagudwog@gmail.com>
2025-03-14 14:56:10 +0900

    PaymentService 개발

---
+++ src/main/java/com/toby/spring_six/ExRateData.java
@@ -0,0 +1,10 @@
+package com.toby.spring_six;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
